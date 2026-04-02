2d4c76117f385ee3b9a9c95ed856f3236bd5a017
Thinh Lam <thinh@thinh-0005-mac-mbam2.local>
2026-04-02 13:48:16 +0700

    test: trigger Atlantis PR plan

--- atlantis_test.tf
+++ atlantis_test.tf
@@ -2,3 +2,7 @@ locals {
   # Test marker to trigger Atlantis plan on PR without changing infrastructure.
   atlantis_pr_test_marker = "test-atlantis-webhook"
 }
+
+locals {
+  atlantis_pr_test_marker_2 = "trigger-1775112496"
+}
